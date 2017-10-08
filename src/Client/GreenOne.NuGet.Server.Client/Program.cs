a7ebda827b5ab2c8e6ca8bd06e8adc198e02326b
michaeldeongreen <michael.green@architectinginnovation.com>
2017-10-07 20:26:02 -0500

    *Successfully added private nuget to vs2017 and installed library in client solution and was able to use it

--- src/Client/GreenOne.NuGet.Server.Client/Program.cs
+++ src/Client/GreenOne.NuGet.Server.Client/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GreenOne.NuGet.Server.Library;
 
 namespace GreenOne.NuGet.Server.Client
 {
@@ -10,6 +11,8 @@ namespace GreenOne.NuGet.Server.Client
     {
         static void Main(string[] args)
         {
+            int result = new AddThem().Add(3, 5);
+            Console.WriteLine(string.Format("Result is {0}", result.ToString()));
             Console.ReadLine();
         }
     }
