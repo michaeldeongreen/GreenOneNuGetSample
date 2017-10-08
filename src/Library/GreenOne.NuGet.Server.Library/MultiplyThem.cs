1be0573c717dd003a37fddc34fcd01e0b444f6a3
michaeldeongreen <michael.green@architectinginnovation.com>
2017-10-07 21:32:20 -0500

    *Added multiply to library and upgraded to version 3.0.0

---
+++ src/Library/GreenOne.NuGet.Server.Library/MultiplyThem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenOne.NuGet.Server.Library
+{
+    public class MultiplyThem
+    {
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
