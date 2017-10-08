b5dbe96be744417c01cdbfb147aee81d0cf18f69
michaeldeongreen <michael.green@architectinginnovation.com>
2017-10-07 19:25:13 -0500

    *Library that will be published to private nuget created

---
+++ src/Library/GreenOne.NuGet.Server.Library/AddThem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenOne.NuGet.Server.Library
+{
+    public class AddThem
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
